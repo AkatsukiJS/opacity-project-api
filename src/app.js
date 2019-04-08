50e36983363af48fa5b64936c3ff1be4bd09c7ec
jeovazero <jeovapereira2000@live.com>
2019-04-08 17:21:12 -0300

    chore: add rate-limiter

--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@ const app = new Koa();
 const PORT = process.env.PORT || 3001;
 const router = require('./routes')
 const logger = require('koa-logger')
+const RateLimit = require('koa2-ratelimit').RateLimit;
 
+const limiter = RateLimit.middleware({
+  interval: { min: 15 },
+  max: 450
+})
+
+app.use(limiter)
 app.use(logger())
 app.use(bodyparser());
 app.use(router.routes());
-
 module.exports = app
