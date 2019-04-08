aec2b1a6d7e8e75b53908d270428a0d6b619fdfa
jeovazero <jeovapereira2000@live.com>
2019-04-08 17:08:18 -0300

    chore: add koa-logger

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const bodyparser = require("koa-bodyparser");
 const app = new Koa();
 const PORT = process.env.PORT || 3001;
 const router = require('./routes')
+const logger = require('koa-logger')
 
+app.use(logger())
 app.use(bodyparser());
 app.use(router.routes());
 
