5db46b822164fad92d7387b49fefb008301c3aed
jeovazero <jeovapereira2000@live.com>
2019-04-10 20:26:29 -0300

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -6,12 +6,17 @@ const router = require('./routes')
 const logger = require('koa-logger')
 const RateLimit = require('koa2-ratelimit').RateLimit;
 const helmet = require('koa-helmet')
+const cors = require('@koa/cors')
+const ORIGIN_ALLOWED = process.env.ORIGIN_ALLOWED || '*'
+
+console.log({ ORIGIN_ALLOWED })
 
 const limiter = RateLimit.middleware({
   interval: { min: 15 },
   max: 450
 })
 
+app.use(cors({ origin: ORIGIN_ALLOWED }))
 app.use(helmet())
 app.use(limiter)
 app.use(logger())
