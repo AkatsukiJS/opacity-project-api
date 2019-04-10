7ae6d90ae0998d792b65da7f898605caf5ced1fb
jeovazero <jeovapereira2000@live.com>
2019-04-10 19:49:56 -0300

    refactor: update get.category and get.info

--- src/routes.js
+++ src/routes.js
@@ -13,19 +13,17 @@ router.get('/', async (ctx) => {
 
 router.get('/categories', async (ctx) => {
   const results = db.getCategories()
-  ctx.body = { results }
+  ctx.body = results
 });
 
 router.get('/info', async (ctx) => {
   ctx.body = {
-    info : {
-      month_year_version: '01-2018',
-      remuneration_dictionary: 'http://www.portaltransparencia.gov.br/pagina-interna/603423-dicionario-de-dados-servidores-remuneracao',
-      register_dictionary: 'http://www.portaltransparencia.gov.br/pagina-interna/603422-dicionario-de-dados-servidores-cadastro',
-      data_source: 'http://www.portaltransparencia.gov.br/download-de-dados/servidores',
-      api_repository: 'https://github.com/AkatsukiJS/opacity-project-api',
-      servers_endpoint: 'http://www.portaltransparencia.gov.br/servidores/:id'
-    }
+    month_year_version: '01-2018',
+    remuneration_dictionary: 'http://www.portaltransparencia.gov.br/pagina-interna/603423-dicionario-de-dados-servidores-remuneracao',
+    register_dictionary: 'http://www.portaltransparencia.gov.br/pagina-interna/603422-dicionario-de-dados-servidores-cadastro',
+    data_source: 'http://www.portaltransparencia.gov.br/download-de-dados/servidores',
+    api_repository: 'https://github.com/AkatsukiJS/opacity-project-api',
+    servers_endpoint: 'http://www.portaltransparencia.gov.br/servidores/:id'
   }
 })
 
